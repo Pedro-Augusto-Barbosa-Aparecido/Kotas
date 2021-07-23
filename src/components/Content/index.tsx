481c48e08afc1d2716a18116c33f98833eed163e
Pedro-Augusto-Barbosa-Aparecido <pedr.augustobarbosa.aparecido.gmail.com>
2021-07-23 10:44:45 -0300

    First commit

---
+++ src/components/Content/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+export default function Content () {
+    return (
+        <div className={styles.container}>
+            <div className={styles.column}>
+                <div className={styles.box}>
+                    <img src="/spotfy.png" alt="logo do spotfy" width="43.2" height="43.2" />
+                </div>
+            </div>
+            <div className={styles.column}>
+                
+            </div>
+
+        </div>
+    );
+}
